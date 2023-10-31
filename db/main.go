c1565f3b5384653164606860e8aaadf7da655153
榎橘征爾 <kakitsuseiji@enokitaijinoAir.elecom>
2023-10-31 19:47:31 +0900

    8080

--- db/main.go
+++ db/main.go
@@ -167,7 +167,7 @@ func main() {
 
 	// 8000番ポートでリクエストを待ち受ける
 	log.Println("Listening...")
-	if err := http.ListenAndServe(":8000", nil); err != nil {
+	if err := http.ListenAndServe("localhost:8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
